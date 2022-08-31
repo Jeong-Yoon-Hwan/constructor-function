2633b8e1f659b59f6be559c4ed4612463f1ed41c
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-08-31 14:41:52 +0900

    method

---
+++ array-method-test.js
@@ -0,0 +1,12 @@
+let pokemon=['피카츄','라이츄','파이리','꼬부기'];
+let arr = [1,2,3,4];
+
+
+let users = [
+  {id: 1, name: "John"},
+  {id: 2, name: "Pete"},
+  {id: 3, name: "Mary"}
+];
+
+let result = users.filter(item => item.name ==="John");
+console.log(result)
